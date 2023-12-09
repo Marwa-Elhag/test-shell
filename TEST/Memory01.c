21c3bce6bc3af435a2416eab8b898e1df765b86d
Marwa-Elhag <marwaalhaj89@gmail.com>
2023-12-09 06:26:41 -0800

    test

---
+++ TEST/Memory01.c
@@ -0,0 +1,63 @@
+#include "shell.h"
+
+/**
+ * *_memset - to fill memory
+ * @s: pointer
+ * @b: byte fo fill
+ * @n: amount of bytes
+ * Return: Success
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
+}
+
+/**
+ * ffree - to free string of strings
+ * @pp: string of strings
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - to reallocate memory
+ * @ptr: pointer
+ * @old_size: previous block size in byte
+ * @new_size: new block size in byte
+ *
+ * Return: success
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int old_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
+
